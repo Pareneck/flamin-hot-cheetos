6fa4eb812e1819c348bb02ef8cbe3f5dd365cc90
Epik <xboxjames123@gmail.com>
2016-06-27 16:17:41 -0700

    Modified default control variable values

--- flamin' hot cheetos/Control Variables.cpp
+++ flamin' hot cheetos/Control Variables.cpp
@@ -7,21 +7,21 @@ namespace cvar
 
 	bool aimbot_enabled = true;
 	float aimbot_fov = 10.f;
-	float aimbot_smoothing = 50.f;
-	float aimbot_rcs_min = 1.8f;
-	float aimbot_rcs_max = 2.0f;
-	float aimbot_randomize_hitbox = 5.f;
-	float aimbot_randomize_angle = 2.f;
+	float aimbot_smoothing = 25.f;
+	float aimbot_rcs_min = 2.f;
+	float aimbot_rcs_max = 2.f;
+	float aimbot_randomize_hitbox = 0.f;
+	float aimbot_randomize_angle = 0.f;
 
 	bool esp_enabled = true;
 	bool esp_draw_box = true;
 	bool esp_draw_name = true;
 	bool esp_draw_weapon = true;
-	bool esp_draw_callout = true;
+	bool esp_draw_callout = false;
 	bool esp_draw_health = true;
-	bool esp_draw_health_text = true;
-	bool esp_draw_armor = true;
-	bool esp_draw_armor_text = true;
+	bool esp_draw_health_text = false;
+	bool esp_draw_armor = false;
+	bool esp_draw_armor_text = false;
 	bool esp_draw_world = true;
 	bool esp_draw_glow = true;
 
